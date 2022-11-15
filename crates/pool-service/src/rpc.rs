af814732c11d773f4f5dfe19a6195086b25e0234
harodggg <haroldtiansheng@gmail.com>
2022-11-15 23:23:48 +0800

    add AeloBlock

--- crates/pool-service/src/rpc.rs
+++ crates/pool-service/src/rpc.rs
@@ -1,19 +1,22 @@
-use std::result;
-
-use block::block_service::{Block, BlockService};
-use block::{BlockRequest, BlockResponse};
-use tonic::{transport::Server, Request, Response, Status};
-
 pub mod block {
     tonic::include_proto!("block"); // The string specified here must match the proto package name
 }
+
+use block::block_server::{Block, BlockServer};
+use block::{BlockRequest, BlockRespone};
+use tonic::{transport::Server, Request, Response, Status};
+
 #[derive(Debug, Default)]
-pub struct Block { }
+pub struct AleoBlock {}
 
 #[tonic::async_trait]
-impl Block { 
-    async fn get_block(&self,request:BlockRequest) ->result<BlockResponse,Status>{
-        Ok()
+impl Block for AleoBlock {
+    async fn get_block(
+        &self,
+        request: Request<BlockRequest>,
+    ) -> Result<Response<BlockRespone>, Status> {
+        println!("{:?}", request);
+        let response = BlockRespone { difficult: 11 };
+        Ok(Response::new(response))
     }
-
 }
