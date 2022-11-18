df703e229c484e6479f757eeae88a9dd3a36ba2b
harodggg <haroldtiansheng@gmail.com>
2022-11-18 20:49:33 +0800

    design stop method

--- crates/pool-client/src/prover.rs
+++ crates/pool-client/src/prover.rs
@@ -1,10 +1,34 @@
-
 // 1,接受块。
 // 2，接受难度。
 // 3，计算难度。
 // 4，提交解决。
 // 5， 不运行全节点，不连接其他网络。
 // 6，把随机的nonce 改为固定的nonce
+use anyhow::Result;
+enum CoinBaseState {
+    Start,
+    Runing,
+    Stop,
+}
 
-
-
+pub fn try_coinbase_solution(
+    from_nonce: u64,
+    to_nonce: u64,
+    last_block: _,
+    last_target: _,
+    state: &Arc<CoinBaseState>,
+) -> Result<String> {
+    for nonce in from..to {
+        match state {
+            Start => {
+                info! {"Start New Coinbase Solution"}
+            }
+            Runing => {
+                info! {"Computing Coinbase Solution,the nonce is {}",nonce}
+            }
+            Stop => {
+                info! {"Stopping Coinbase"}
+            }
+        }
+    }
+}
