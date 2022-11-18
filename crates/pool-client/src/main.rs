ac75b5751f0b6b68faaeee9dff6359a6ce6cad32
harodggg <haroldtiansheng@gmail.com>
2022-11-18 19:42:13 +0800

    add run funciton

--- crates/pool-client/src/main.rs
+++ crates/pool-client/src/main.rs
@@ -1,4 +1,43 @@
+use clap::Parser;
+use simple_log::LogConfigBuilder;
+use simple_log::{debug, info, warn};
 
-fn main() {
-    println!("Hello, world!");
+#[derive(clap::ValueEnum, Clone)]
+enum State {
+    Run,
+    Stop,
+    Pause,
+}
+
+/// Aelo Mining pool service program
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Starting a aleo mining pool service
+    #[arg(long)]
+    start: bool,
+
+    /// Stoping a aleo mining pool service
+    #[arg(long)]
+    stop: bool,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::parse();
+    let config = LogConfigBuilder::builder()
+        .path("./log/builder_log.log")
+        .size(1 * 100)
+        .roll_count(10)
+        .time_format("%Y-%m-%d %H:%M:%S.%f") //E.g:%H:%M:%S.%f
+        .level("debug")
+        .output_file()
+        .output_console()
+        .build();
+    simple_log::new(config)?;
+    debug!("test builder debug");
+
+    if args.start {
+        println!("start");
+    }
+    Ok(())
 }
