12979f6d568124c864362ec39fa1820aad79c7f8
harodggg <haroldtiansheng@gmail.com>
2022-11-19 20:05:22 +0800

    Preliminary completion of stratum worker rpc service design

--- crates/stratum-worker/src/lib.rs
+++ crates/stratum-worker/src/lib.rs
@@ -1,14 +1,46 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub mod stratum_worker {
+    tonic::include_proto!("stratum_worker");
+}
+use anyhow::Result;
+use stratum_worker::stratum_worker_server::{StratumWorker, StratumWorkerServer};
+use stratum_worker::{DifficultRequest, DifficultRespone};
+use stratum_worker::{NotifyRequest, NotifyRespone};
+use tonic::transport::Server;
+use tonic::{Request, Response, Status};
+
+#[derive(Debug, Default)]
+pub struct AleoStratumWorker {}
+
+#[tonic::async_trait]
+impl StratumWorker for AleoStratumWorker {
+    async fn mining_set_difficult(
+        &self,
+        request: Request<DifficultRequest>,
+    ) -> Result<Response<DifficultRespone>, Status> {
+        println!("{:?}", request);
+        let respone = DifficultRespone { id: 1 };
+        Ok(Response::new(respone))
+    }
+
+    async fn mining_notify(
+        &self,
+        request: Request<NotifyRequest>,
+    ) -> Result<Response<NotifyRespone>, Status> {
+        println!("{:?}", request);
+        let respone = NotifyRespone { id: 1 };
+        Ok(Response::new(respone))
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub async fn run_stratum_service() -> Result<()> {
+    let addr = "[::1]:50051".parse()?;
+    println!("Starting Stratum Service");
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    let stratum = AleoStratumWorker::default();
+    Server::builder()
+        .add_service(StratumWorkerServer::new(stratum))
+        .serve(addr)
+        .await?;
+
+    Ok(())
 }
