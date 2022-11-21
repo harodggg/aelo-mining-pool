84cae67779d42ae561fe632dd9a5bf444bd3e938
G-XBT <31732456+harodggg@users.noreply.github.com>
2022-11-21 12:11:53 +0800

    Feature fetching blocks continuously (#13)
    * Add AleoBlock desgin
    
    * Implement the observer mode to solve the block subscription problem
    
    * init

--- crates/aleo-client/src/main.rs
+++ crates/aleo-client/src/main.rs
@@ -1,6 +1,23 @@
-fn main() {
-    // todo 开放接口，将数据写到数据中，供mining pool读取。
-    // todo 可以通过 grpc 进行。使用3个grpc 服务进行通信。以实现代码分离和抽象。
-    // todo 独立二进制程序，不与pool service 一起启动，实现解除耦合。
-    // todo rpc 读取，和写数据库。
-}
+
+use simple_log::{LogConfigBuilder, log::debug, info};
+
+// todo 开放接口，将数据写到数据中，供mining pool读取。
+// todo 可以通过 grpc 进行。使用3个grpc 服务进行通信。以实现代码分离和抽象。
+// todo 独立二进制程序，不与pool service 一起启动，实现解除耦合。
+// todo rpc 读取，和写数据库。
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let config = LogConfigBuilder::builder()
+        .path("./log/builder_log.log")
+        .size(1 * 100)
+        .roll_count(10)
+        .time_format("%Y-%m-%d %H:%M:%S.%f") //E.g:%H:%M:%S.%f
+        .level("debug")
+        .output_file()
+        .output_console()
+        .build();
+    simple_log::new(config)?;
+    debug!("test builder debug");
+
+    info!("Runing Stratum Service");
+    Ok(())
