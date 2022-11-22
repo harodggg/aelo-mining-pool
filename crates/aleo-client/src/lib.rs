1efae3d933ff1c4ec283f43bb8e9bc27df45f26f
G-XBT <31732456+harodggg@users.noreply.github.com>
2022-11-23 00:01:59 +0800

    Dev6 (#21)
    * fix something error,but have a connect error
    
    * fix somethings
    
    * No solve connect Problem

--- crates/aleo-client/src/lib.rs
+++ crates/aleo-client/src/lib.rs
@@ -1,15 +1,8 @@
 mod client;
-
-mod node_interface;
-//use node_interface::*;
-
-use std::alloc;
-use std::str;
-use std::string;
-use std::usize;
+pub use client::*;
+use snarkos_node::Node;
 
 // 实现观察者模式。
-
 struct block {}
 
 pub trait Observer {
