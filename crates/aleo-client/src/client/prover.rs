fb2dc9fa5d5797c44628da52840950902f2490b2
harodggg <haroldtiansheng@gmail.com>
2022-11-23 16:58:03 +0800

    fix connect error

--- crates/aleo-client/src/client/prover.rs
+++ crates/aleo-client/src/client/prover.rs
@@ -1,3 +1,4 @@
+use anyhow::Result;
 use core::str::FromStr;
 use snarkos_node::Node;
 use snarkvm::prelude::{PrivateKey, Testnet3};
@@ -19,26 +20,16 @@ const BOOTSTRAP: [&str; 6] = [
     "161.35.24.55:4133",
 ];
 
-pub async fn run_node_prover() {
+pub async fn run_node_prover() -> Result<()> {
     Node::new_prover(
-        SocketAddr::from_str(NODE_IP).unwrap(),
-        PrivateKey::<CurrentNetwork>::from_str(PRIVATE_KEY).unwrap(),
-        &[
-            SocketAddr::from_str("164.92.111.59:4133").unwrap(),
-            SocketAddr::from_str("159.223.204.96:4133").unwrap(),
-            SocketAddr::from_str("167.71.219.176:4133").unwrap(),
-            SocketAddr::from_str("157.245.205.209:4133").unwrap(),
-            SocketAddr::from_str("134.122.95.106:4133").unwrap(),
-            SocketAddr::from_str("161.35.24.55:4133").unwrap(),
-            SocketAddr::from_str("138.68.103.139:4133").unwrap(),
-            SocketAddr::from_str("207.154.215.49:4133").unwrap(),
-            SocketAddr::from_str("46.101.114.158:4133").unwrap(),
-            SocketAddr::from_str("138.197.190.94:4133").unwrap(),
-        ],
+        SocketAddr::from_str(NODE_IP)?,
+        PrivateKey::<CurrentNetwork>::from_str(PRIVATE_KEY)?,
+        &[],
         None,
     )
-    .await
-    .unwrap();
+    .await?;
+
+    Ok(())
 }
 pub fn runtime() -> Runtime {
     // TODO (howardwu): Fix this.
