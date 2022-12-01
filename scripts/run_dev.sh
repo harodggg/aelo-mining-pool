d7df6490ad1bb5150afe1f11eddda410340b2d39
harodggg <haroldtiansheng@gmail.com>
2022-12-01 18:34:26 +0800

    somethings

--- scripts/run_dev.sh
+++ scripts/run_dev.sh
@@ -3,8 +3,12 @@ SNARK_PATH="../../snarkOS"
 WORK_PATH=$(pwd)
 
 cd $SNARK_PATH
-nohup cargo run --release -- start --nodisplay --dev 0 --beacon "" >beacon.log 2>&1 &
-nohup cargo run --release -- start --nodisplay --dev 1 --prover "" >prover.log 2>&1 &
+nohup cargo run --release -- start --nodisplay --dev 0 --beacon "" > ./beacon.log 2>&1 &
+nohup cargo run --release -- start --nodisplay --dev 1 --prover "" > ./prover.log 2>&1 &
 cd $(pwd)
+
+function kill_dev { 
+
+}
 
 
