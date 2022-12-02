27ea132caa698b93447fe152d7f5e3d199afefa7
harodggg <haroldtiansheng@gmail.com>
2022-12-02 13:41:08 +0800

    add run_dev

--- scripts/run_dev.sh
+++ scripts/run_dev.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
-SNARK_PATH="../../snarkOS"
+SNARK_PATH="$HOME/snarkOS"
 WORK_PATH=$(pwd)
 
-cd $SNARK_PATH
-nohup cargo run --release -- start --nodisplay --dev 0 --beacon "" > ./beacon.log 2>&1 &
-nohup cargo run --release -- start --nodisplay --dev 1 --prover "" > ./prover.log 2>&1 &
-cd $(pwd)
-
-function kill_dev { 
-
+function start_up {
+    if [ $1 == 1 ]; then
+        cd $SNARK_PATH
+        echo "Start up beacon"
+        nohup cargo run --release -- start --nodisplay --dev 0 --beacon "" >$SNARK_PATH/beacon.log 2>&1 &
+        echo "Start up prover"
+        nohup cargo run --release -- start --nodisplay --dev 1 --prover "" >$SNARK_PATH/prover.log 2>&1 &
+        cd $(pwd)
+    fi
 }
 
+function kill_devnet {
+    if [ $1 == 0 ]; then
+        echo "Killall beacon and prover"
+        killall snarkos
+        echo "clear beacon log and prover log"
+        rm $SNARK_PATH/beacon.log $SNARK_PATH/prover.log
 
+    fi
+}
+
+start_up $1
+kill_devnet $1
