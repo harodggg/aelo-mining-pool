7f234791fd433ec85cc8674c3bccee5ba373c190
harodggg <haroldtiansheng@gmail.com>
2022-12-10 15:59:12 +0800

    fix somethings

--- scripts/test_stratum_service.sh
+++ scripts/test_stratum_service.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-grpcurl -plaintext -import-path  ../crates/protos -proto stratum.proto -d '{"id": 10231}' '[::]:50051' stratum.Stratum.MiningShare
+WORK_PATH="/Users/g/aelo-mining-pool"
+grpcurl -plaintext -import-path $WORK_PATH/crates/protos -proto stratum-pool.proto -d '{"id": 10231}' '[::]:50051' stratum_pool.StratumPool.MiningAuthorize
