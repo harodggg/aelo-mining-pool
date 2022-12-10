dbf2953cb56f0610e858208cfaf9cc4642ff1c2f
harodggg <haroldtiansheng@gmail.com>
2022-12-10 14:25:09 +0800

    delete somethings

--- crates/aleo-pool/src/db.rs
+++ crates/aleo-pool/src/db.rs
@@ -20,61 +20,21 @@ const CACHE_POOL_TIMEOUT_SECONDS: u64 = 1;
 const CACHE_POOL_EXPIRE_SECONDS: u64 = 60;
 const REDIS_CON_STRING: &str = "redis://127.0.0.1:6379";
 
-pub async fn connect() -> Result<MobcPool> {
-    let client = redis::Client::open(REDIS_CON_STRING)?;
-    let manager = RedisConnectionManager::new(client);
-    Ok(Pool::builder()
-        .get_timeout(Some(Duration::from_secs(CACHE_POOL_TIMEOUT_SECONDS)))
-        .max_open(CACHE_POOL_MAX_OPEN)
-        .max_idle(CACHE_POOL_MAX_IDLE)
-        .max_lifetime(Some(Duration::from_secs(CACHE_POOL_EXPIRE_SECONDS)))
-        .build(manager))
-}
-
-async fn get_con(pool: &MobcPool) -> Result<MobcCon> {
-    unimplemented!()
-}
-
-pub async fn set_str(pool: &MobcPool, key: &str, value: &str, ttl_seconds: usize) -> Result<()> {
-    let mut con = get_con(&pool).await?;
-
-    Ok(())
-}
-
-pub async fn get_str(pool: &MobcPool, key: &str) -> Result<String> {
-    let mut con = get_con(&pool).await?;
-    //FromRedisValue::from_redis_value(&value).map_err(|e| Error(e).into())
-    Ok(String::from("rrl"))
-}
-
-pub fn add_woker() -> Result<String, ()> {
-    Ok("harold".to_string())
-}
-
-pub fn get_block() {
-    unimplemented!()
-}
-
-pub fn get_blockhead() {
-    unimplemented!()
-}
-
-pub fn get_worker() {
-    unimplemented!()
-}
-
 struct DB {
     // The redis connect pool
     redis_pool: Pool<RedisConnectionManager>,
 }
 
 impl DB {
-    pub fn connect_db(&mut self) -> Result<bool> {
-        let client = redis::Client::open("redis://127.0.0.1:6379").unwrap();
+    pub async fn connect_db(&mut self) {
+        let client = redis::Client::open(REDIS_CON_STRING).expect("redis connect error");
         let manager = RedisConnectionManager::new(client);
-        let pool = Pool::builder().max_open(20).build(manager);
-        self.redis_pool = pool;
-        Ok(true)
+        self.redis_pool = Pool::builder()
+            .get_timeout(Some(Duration::from_secs(CACHE_POOL_TIMEOUT_SECONDS)))
+            .max_open(CACHE_POOL_MAX_OPEN)
+            .max_idle(CACHE_POOL_MAX_IDLE)
+            .max_lifetime(Some(Duration::from_secs(CACHE_POOL_EXPIRE_SECONDS)))
+            .build(manager);
     }
 
     pub fn store_worker(&mut self, worker_name: &str, work_content: &str) -> Result<bool> {
